ba740ea2220decd1cdbf90e27dc70f270f80609d
Deric Siu <dddsiu@Derics-MacBook-Pro.local>
2022-05-11 18:37:22 -0700

    Send products as json to client.

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const http = require('http');
+const products = require('./data/products.json');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello Doofus</h1>');
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/html');
+  // Better way of setting statusCode and header:
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify(products));
   res.end();
 });
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Running on PORT ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
